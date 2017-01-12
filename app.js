6efafd7b9172988bc99e6f8d36936aa9bd1092f1
brucse <brucse@gmail.com>
2017-01-12 16:21:21 +0000

    server.js set to c9 and openshift either

--- app.js
+++ app.js
@@ -4,38 +4,6 @@ var app = express()
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/cease.html', function (req, res) {
   console.log('req')
-  res.send('
-  <!doctype html>
-  <html lang="en">
-
-  <head>
-  <meta charset="UTF-8" />
-  <title>Catch the robber</title>
-  <script type="text/javascript" src="js/phaser.min.js"></script>
-  <script type="text/javascript" src="js/lodash.js"></script>
-  <script type="text/javascript" src="js/lib.js"></script>
-  <script type="text/javascript" src="data/survey_data.js"></script>
-  <script type="text/javascript" src="js/variables.js"></script>
-  <script type="text/javascript" src="js/preload.js"></script>
-  <script type="text/javascript" src="js/create.js"></script>
-  <script type="text/javascript" src="js/script.js"></script>
-  <script type="text/javascript" src="js/utils.js"></script>
-  <style type="text/css">
-  body {
-  	margin: 0;
-  }
-  </style>
-  </head>
-
-  <body>
-  	<a href="">Ujra</a>
-
-  </body>
-
-  </html>
- 
-  ')
-  
 })
 
 var router = express.Router()
@@ -44,6 +12,7 @@ router.use(function(req,res,next){
 })
 app.use('/',express.static('public'))
 app.use('/',router)
-app.listen(3000, function () {
+// app.listen(3000, function () {
+app.listen(process.env.PORT, process.env.IP, function () {
   console.log('Example app listening on port 3000!')
 })
