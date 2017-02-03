192cdc9202d685b9fb1dbb6b587e5f8db2354db1
brucse <brucse@gmail.com>
2017-02-03 15:50:22 +0000

    SurveyState implemented as car count, first step

--- public/js/game/SurveyState.js
+++ public/js/game/SurveyState.js
@@ -2,7 +2,7 @@ PracticeGame.SurveyState = function(game) {
     this.carStockCount = 11
     this.parking_lot
     this.carList = []
-    this.carCount = 2
+    this.carCount = 4
 
 };
 
@@ -41,49 +41,101 @@ PracticeGame.SurveyState.prototype.create = function() {
     var stockGapX = 60
     var rowNum = 4
     var columnNum = Math.ceil(this.carStockCount / rowNum)
-    for(var j = 0; j < columnNum; j++){
-        for(var k = 0; k < rowNum; k++){
-        if((j+1) * (k+1) > this.carStockCount){
-          break  
-        } 
-            
-        var car = this.carGroup.create(halfLine + 100 + j * stockGapX, 50 + k * stockGap, 'cop_car')
-        car.inputEnabled = true;
-        car.input.enableDrag();
-        car.events.onDragStop.add(this.utils.onDragStop, this);
-        this.carList.push(car)
-            
+    for (var j = 0; j < columnNum; j++) {
+        for (var k = 0; k < rowNum; k++) {
+            if ((j + 1) * (k + 1) > this.carStockCount) {
+                break
+            }
+
+            var car = this.carGroup.create(halfLine + 100 + j * stockGapX, 50 + k * stockGap, 'cop_car')
+            car.inputEnabled = true;
+            car.input.enableDrag();
+            car.events.onDragStop.add(this.utils.onDragStop, this);
+            this.carList.push(car)
+
         }
     }
-/*    for (var i = 0; i < this.carStockCount; i++) {
-        columnNum = Math.floor(i / 4)
-        var car = this.carGroup.create(halfLine + 100 + columnNum * stockGapX, 50 + i * stockGap, 'cop_car')
-        car.inputEnabled = true;
-        car.input.enableDrag();
-        car.events.onDragStop.add(this.utils.onDragStop, this);
-        this.carList.push(car)
-    }
-*/
+    /*    for (var i = 0; i < this.carStockCount; i++) {
+            columnNum = Math.floor(i / 4)
+            var car = this.carGroup.create(halfLine + 100 + columnNum * stockGapX, 50 + i * stockGap, 'cop_car')
+            car.inputEnabled = true;
+            car.input.enableDrag();
+            car.events.onDragStop.add(this.utils.onDragStop, this);
+            this.carList.push(car)
+        }
+    */
 
     this.carGroup.inputEnableChildren = true;
 
     this.referenceCarGroup = this.add.group()
 
-    var gap = 30
-    for (var i = 0; i < this.carCount; i++) {
-        this.referenceCarGroup.create(10, 300 + i * gap, 'cop_car')
+    var gapX = 70
+    var gapY = 30
+    // var xMax = 50
+    // var yMax = 50
+    var centerX = 30
+    var centerY = 300
+
+
+
+    var positionGrid = []
+    var gridWidth = 4 
+    var gridHeight = 4 
+
+    //should handle as grid, for domino etc
+    //create position grid  
+    for (var i = 0; i < gridHeight; i++) {
+        var row = []
+        for (var j = 0; j < gridWidth; j++) {
+            row[j] = null 
+        }
+        positionGrid[i] = row 
     }
 
+    //calculate position grid elements' coordinates
 
-    //  Enable input and allow for dragging
-/*    this.car1.inputEnabled = true;
-    this.car1.input.enableDrag();
-    this.car2.inputEnabled = true;
-    this.car2.input.enableDrag();
-    // this.car1.events.onDragStart.add(this.utils.onDragStart, this);
-    this.car1.events.onDragStop.add(this.utils.onDragStop, this);
-    this.car2.events.onDragStop.add(this.utils.onDragStop, this);
-*/
+    var GridCoordinate = {
+        x: null,
+        y: null
+    }
+
+    for (var i = 0; i < gridWidth; i++) {
+        for (var j = 0; j < gridHeight; j++) {
+            var gridCoordinate = Object.create(GridCoordinate)
+            gridCoordinate.x = centerX + i * gapX
+            gridCoordinate.y = centerY + j * gapY
+            positionGrid[i][j] = gridCoordinate
+        }
+    }
+
+    //find grid cells randomly
+
+    var gridCellNumber = gridWidth * gridHeight
+
+    var arr = []
+    while (arr.length < this.carCount) {
+        var randomnumber = Math.ceil(Math.random() * (gridCellNumber - 1))
+        if (arr.indexOf(randomnumber) > -1) continue;
+        arr[arr.length] = randomnumber;
+    }
+    
+    console.log('arr ' + arr)
+
+    var flattenend = _.flatten(positionGrid)
+    for(var i = 0; i < arr.length; i++){
+       this.referenceCarGroup.create(flattenend[arr[i]].x,flattenend[arr[i]].y,'cop_car') 
+    }
+
+    // previousElement = this.referenceCarGroup.create(actualX, actualY, 'cop_car')
+        //  Enable input and allow for dragging
+        /*    this.car1.inputEnabled = true;
+            this.car1.input.enableDrag();
+            this.car2.inputEnabled = true;
+            this.car2.input.enableDrag();
+            // this.car1.events.onDragStart.add(this.utils.onDragStart, this);
+            this.car1.events.onDragStop.add(this.utils.onDragStop, this);
+            this.car2.events.onDragStop.add(this.utils.onDragStop, this);
+        */
 
 }
 
