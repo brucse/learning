326498419d649f708f9addaef4d6b08ecdb71acd
brucse <brucse@gmail.com>
2017-03-27 14:36:30 +0000

    multiplying with 2 , first raw version

--- public/js/game/SurveyStateLearn2X.js
+++ public/js/game/SurveyStateLearn2X.js
@@ -5,9 +5,17 @@ PracticeGame.SurveyStateLearn2X = function(game) {
     // this.carCount = 4
     this.instructionText 
     this.questionText
-    this.bicyleOnClick
+    this.magicianButton
     this.bicycleGroup
+    this.bicycleNoWheelGroup
+    this.bicycleWheelGroup
     this.bicycleCount = 0
+    this.readyButton
+    this.computeButton
+    this.helpButton
+    this.helpCountingTextGroup
+    this.helpCountingTextGroupSetting
+    this.answer = "" 
 
 };
 
@@ -16,9 +24,14 @@ PracticeGame.SurveyStateLearn2X.prototype = Object.create(PracticeGameBaseState.
 PracticeGame.SurveyStateLearn2X.prototype.preload = function() {
 
     this.load.spritesheet('bicycle', 'assets/bicycle.png', 120, 76);
+    this.load.spritesheet('bicycle_no_wheel', 'assets/bicycle_no_wheel.png', 120, 76);
+    this.load.spritesheet('bicycle_wheel', 'assets/bicycle_wheel.png', 46, 47);
+    this.load.spritesheet('magician', 'assets/magician.png', 60, 49);
     this.load.spritesheet('cop_car', 'assets/cop-car_small.png', 60, 24);
     this.load.spritesheet('parking_lot', 'assets/parking_lot.png', 300, 150);
     this.load.spritesheet('ready_button', 'assets/ready_button_sheet.png', 45, 23);
+    this.load.spritesheet('compute_button', 'assets/compute_button_sheet.png', 72, 23);
+    this.load.spritesheet('help_button', 'assets/help_button_sheet.png', 72, 23);
     this.load.image('wall', 'assets/sky1.png');
 }
 
@@ -27,16 +40,17 @@ PracticeGame.SurveyStateLearn2X.prototype.create = function() {
     this.wall = this.add.sprite(0, 0, 'wall');
     console.log('start')
     //add instruction text
-    this.instructionText = this.game.add.text(this.game.world.centerX, 20, "Kattints annyiszor a képre,\nahány bicikli kell a szorzáshoz!", {
+    this.instructionText = this.game.add.text(this.game.world.centerX, 20, "Ha tudod az eredményt, írd be és nyomd meg a \"Kész\" gombot. \nHa nem, nyomd meg a \"Segíts\" gombot", {
+    // this.instructionText = this.game.add.text(this.game.world.centerX, 20, "Kattints annyiszor a képre,\nahány bicikli kell a szorzáshoz!", {
     // this.instructionText = this.game.add.text(30, 30, "- Kattints annyiszor a képre, ahány bicikli kell a szorzáshoz!", {
-        font: "30px Arial",
+        font: "25px Arial",
         fill: "yellow",
         align: "center"
     });
 
     this.instructionText.anchor.setTo(0.5, 0);
 
-    this.questionText= this.game.add.text(this.game.world.centerX, 220, "5 * 2 = ?", {
+    this.questionText= this.game.add.text(this.game.world.centerX, 120, "5 * 2 = ?", {
         font: "30px Arial",
         fill: "yellow",
         align: "center"
@@ -45,12 +59,33 @@ PracticeGame.SurveyStateLearn2X.prototype.create = function() {
 
     this.physics.startSystem(Phaser.Physics.ARCADE);
 
-    // this.ready_button= this.add.sprite(halfLine,this.world.height - 30, 'ready_button');
+    this.readyButton= this.add.button(halfLine- 50,110, 'ready_button',this.utils.readyButtonClick,this,0,0,0,1);
+    // this.computeButton = this.add.button(halfLine + 50,110, 'help_button',this.utils.helpButtonClick,this,0,0,1,0);
+    this.helpButton = this.add.button(halfLine + 50,110, 'help_button',this.utils.helpButtonClick,this,0,0,1,0);
 
     // this.ready_button = this.add.button(halfLine, this.world.height - 30, 'ready_button', this.utils.clickOnReady, this, 0, 0, 1);
-    this.bicyleOnClick= this.add.button(halfLine - 60, 90, 'bicycle', this.utils.clickOnBicycle, this, 0, 0, 1);
-    this.bicyleOnClick= this.add.button(halfLine - 60, 90, 'ready_button', this.utils.calculate, this, 0, 0, 1);
+    this.magicianButton= this.add.button(halfLine - 60, 190, 'magician', this.utils.clickOnMagician, this, 0, 0, 1);
+    this.magicianButton.visible = false
+    // this.bicyleOnClick= this.add.button(halfLine - 60, 90, 'ready_button', this.utils.calculate, this, 0, 0, 1);
+    
+    
+    this.computeButton = this.add.button(halfLine - 60, 190, 'compute_button', this.utils.computeButtonClick, this, 0, 0, 1);
+    this.computeButton.visible = false
     this.bicycleGroup = this.add.group()
+    this.bicycleGroup.scale.setTo(0.5,0.5)
+    this.bicycleNoWheelGroup = this.add.group()
+    this.bicycleNoWheelGroup.scale.setTo(0.5,0.5)
+    this.bicycleWheelGroup = this.add.group()
+    this.bicycleWheelGroup.scale.setTo(0.5,0.5)
+    this.helpCountingTextGroup = this.add.group()
+    this.helpCountingTextGroupSetting= {
+				font : "25px Arial",
+				fill : "yellow",
+				align : "left"
+		}
+		
+	this.input.keyboard.addCallbacks(this, null, null, this.utils.keyPress);
+	
     // this.bicyleOnClick.setTo(0.5,0)
     // this.parking_lot_X = this.world.width - 300 - 45
     // this.parking_lot_Y = this.world.height / 2
@@ -201,25 +236,81 @@ PracticeGame.SurveyStateLearn2X.prototype.utils = {
 
     },
     
-    clickOnBicycle: function(){
-        console.log("click on bicycle")
+    clickOnMagician: function(){
         if(this.bicycleCount < 10){
         var bicycleLineNum = 0
         var computerImageXCount = 0
         if(this.bicycleCount >= 5){
-           bicycleLineNum = 1 
-           computerImageXCount = 5 
+        //   bicycleLineNum = 1 
+        //   computerImageXCount = 5 
         }
-       var bicycle = this.bicycleGroup.create((this.bicycleCount - computerImageXCount) * 140, 400 + 90 * bicycleLineNum, 'bicycle')
+       var bicycle = this.bicycleGroup.create((this.bicycleCount - computerImageXCount) * 140, 500 , 'bicycle')
        this.bicycleCount++
             
         }
+        if(this.bicycleCount == 10){
+            this.instructionText.setText("Ha segítség kell a számoláshoz, kattints a \"Számol\" gombra.")
+            this.magicianButton.destroy()
+            this.helpButton.destroy()
+            this.readyButton.destroy()
+            this.computeButton.visible = true
+        }
     },
     
-    calculate: function(){
-        this.questionText.setText('5 * 2 = 10')
-    }
+    // calculate: function(){
+    //     this.questionText.setText('5 * 2 = 10')
+    // },
+    
+    helpButtonClick : function(){
+       this.magicianButton.visible = true
+       this.instructionText.setText("Kattints 5-ször a bűvész cilinderére és kapsz öt biciklit.\n Ha megszámolod a kerekeket, megkapod az eredményt.")
+        
+    },
+    
+    computeButtonClick : function(){
+        this.bicycleGroup.destroy()
+        var halfLine = this.world.width / 2
+        for( var i = 0 ; i < this.bicycleCount; i++) {
+            var bicycleLineNum = 0
+            var computerImageXCount = 0
+            if(i >= 5){
+        //   bicycleLineNum = 1 
+        //   computerImageXCount = 5 
+            }
+           var bicycle = this.bicycleNoWheelGroup.create((i - computerImageXCount) * 140, 500 + 90 * bicycleLineNum, 'bicycle_no_wheel')
+        }
+        
+        for(var i = 0; i < this.bicycleCount * 2; i++){
+            var bicycleLineNum = 0
+            var computerImageXCount = 0
+            if(i >= 10){
+          bicycleLineNum = 1 
+          computerImageXCount = 10
+            }
+           var bicycleWheel = this.bicycleWheelGroup.create((i - computerImageXCount)  * 50, 600  + 50 * bicycleLineNum, 'bicycle_wheel')
+        }
+        
+        this.add.text(400,300,"10",this.helpCountingTextGroupSetting)
+        this.add.text(400,350,"20",this.helpCountingTextGroupSetting)
+       this.instructionText.setText("Most már könnyű megszámolni. Írd be az eredményt, azután nyomd meg a \"Kész\" gombot!")
+       this.readyButton= this.add.button(halfLine- 50,110, 'ready_button',this.utils.readyButtonClick,0,0,1);
+       this.computeButton.destroy()
+        
+    },
+    
+    keyPress : function(char){
+       this.answer  = this.answer + char
+       console.log(this.answer)
+       this.questionText.setText("2 * 5 = " + this.answer)
+    },
+    
+   readyButtonClick : function() {
+       if(this.answer == '10'){
+           this.instructionText.setText("Ügyes vagy, ez a helyes válasz!")
+       }
+   }
 
+    
 }
 
 
