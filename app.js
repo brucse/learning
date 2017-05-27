5ce380833d6a0d6c9df6cc54be2c4e172220e6fc
brucse <brucse@gmail.com>
2017-05-27 23:15:25 +0200

    new concept

--- app.js
+++ app.js
@@ -8,11 +8,14 @@ app.get('/cease.html', function (req, res) {
 
 var router = express.Router()
 router.use(function(req,res,next){
-	
+
 })
 app.use('/',express.static('public'))
 app.use('/',router)
+if(process.env.PORT == null) process.env.PORT = '3000'
+if(process.env.IP == null) process.env.IP = '127.0.0.1'
 // app.listen(3000, function () {
+// app.listen(function () {
 app.listen(process.env.PORT, process.env.IP, function () {
   console.log('Example app listening on port 3000!')
 })
