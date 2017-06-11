4e8de76779f0f46a6bb933377b51412401c6e756
brucse <brucse@gmail.com>
2017-06-11 22:13:14 +0200

    first torpedo shooting

--- public/js/game/PracticeState.js
+++ public/js/game/PracticeState.js
@@ -20,12 +20,12 @@ PracticeState.prototype.init = function() {
 PracticeState.prototype.preload = function() {
     this.load.spritesheet('uss_submarine', 'assets/uss_sub.png', 100, 23);
     this.load.spritesheet('battleship', 'assets/battleship.png', 100, 23);
+    this.load.spritesheet('torpedo', 'assets/torpedo.png', 100, 23);
 
     this.load.image('wall', 'assets/sky1.png');
 }
 
 PracticeState.prototype.create = function() {
-    console.log('create')
 
     this.game.physics.startSystem(Phaser.Physics.ARCADE);
     this.wall = this.add.sprite(0, 0, 'wall');
@@ -59,10 +59,14 @@ PracticeState.prototype.create = function() {
         // battleship1.addChild(text1)
         // text1.alignTo(battleship1, Phaser.TOP_CENTER, 16);
         battleship1.label = text
-        battleship1.events.onInputDown.add(this.clickOnBattleShip,battleship1)
+        // battleship1.events.onInputDown.add(this.clickOnBattleShip,battleship1)
+        battleship1.events.onInputDown.add(this.clickOnBattleShip,this)
     }
     this.game.physics.arcade.enable(this.battleships)
     this.questionText = this.game.add.text(this.world.width /2, 30,this.question,style)
+
+
+    this.uss_submarineMove = this.game.add.tween(this.uss_submarine)
 }
 
 PracticeState.prototype.update = function () {
@@ -71,4 +75,17 @@ PracticeState.prototype.update = function () {
 
 PracticeState.prototype.clickOnBattleShip = function (source) {
     console.log(source.label)
+    var y = source.position.y
+    this.uss_submarineMove.to({x:source.position.x})
+    this.uss_submarineMove.onComplete.add(this.startTorpedo,this,null,y)
+    this.uss_submarineMove.start()
+
+
+}
+
+PracticeState.prototype.startTorpedo = function (a,b,y) {
+    var torpedo = this.add.sprite(this.uss_submarine.x, this.uss_submarine.y , 'torpedo')
+   var torpedoTween  = this.game.add.tween(torpedo)
+    torpedoTween.to({y: y})
+    torpedoTween.start()
 }
